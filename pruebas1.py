9e7c275ade9c570c092ff9bcce4dc67664309d53
aliSg98 <ali_sg@outlook.com>
2024-07-12 12:59:57 +0200

    first commit

---
+++ pruebas1.py
@@ -0,0 +1,27 @@
+ca = """holl
+lflflflf
+flffñ"""
+#print (ca)
+d = "df dfsfdsf sfd dfsdf dsf "
+data = d.encode('utf8')
+#print(data)
+
+#listas
+lista = []
+lista.append('apple')
+#sprint(lista)
+#diccionarios
+d = {'p':'48398','a':'389489','g':'87474'}
+print(d['p'])
+
+b={}
+
+#for loop
+cities = ['paris','london','madrid']
+for city in cities:
+    print(city)
+
+tel = {'p':'48398','a':'389489','g':'87474'}
+for t in tel:
+    print(t,tel[t])
+
