0b803d77cb7d2bd65e388f70f553c2373c897780
aliSg98 <ali_sg@outlook.com>
2024-07-14 20:50:06 +0200

    exceptions

---
+++ main.py
@@ -0,0 +1,4 @@
+from exceptions import convert
+
+if __name__ == "__main__":
+    convert("two nine".split())
---
+++ pruebas2.py
@@ -0,0 +1,15 @@
+DIGIT_MAP = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+for clave,valor in DIGIT_MAP.items():
+    print(f"Clave: {clave}, Valor:{valor}")
