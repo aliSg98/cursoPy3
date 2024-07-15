398f2f6fe6d15e97d7ca7c60cbaaaf7ef11b29b8
aliSg98 <ali_sg@outlook.com>
2024-07-15 10:07:07 +0200

    zip

--- iterators.py
+++ iterators.py
@@ -1,3 +1,5 @@
+from itertools import islice
+
 lista = ["summer", "winter", "Autum", "Spring"]
 def iteration(iterable):
     iterable=iter(iterable)
@@ -8,3 +10,17 @@ def iteration(iterable):
     
 iteration(lista)
 
+#####islice
+#crear iterable
+data = range(10)
+#desde el 2 al 7 sin incluir el 7
+res = islice(data,2,7)
+#convierto a lista
+print(list(res))
+
+
+####zip
+numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers2 = [12,34,5,6,7,8,9,10,11]
+for item in zip(numbers,numbers2):
+    print(item)
