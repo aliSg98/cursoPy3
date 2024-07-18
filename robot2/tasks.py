e4f4d77a057d093354c1fcc8116672717283002f
aliSg98 <ali_sg@outlook.com>
2024-07-18 21:06:12 +0200

    robot2_2

--- robot2/tasks.py
+++ robot2/tasks.py
@@ -1,5 +1,5 @@
 from robocorp.tasks import task
-from robocorp import browser
+from robocorp import browser, vault
 from RPA.HTTP import HTTP
 from RPA.Tables import Tables
 
@@ -13,17 +13,25 @@ def order_robots_from_RobotSpareBin():
     Creates ZIP archive of the receipts and the images.
     """
     browser.configure(
-        slowmo=100,
+        slowmo=10000,
     )
     open_robot_order_website()
-    orders = get_orders()
+    log_in()
+    get_orders()
     #close_annoying_modal()
-    fill_the_form()
+    #fill_the_form()
 
 
 def open_robot_order_website():
     """Navigates to the given URL"""
     browser.goto("https://robotsparebinindustries.com/")
+
+def log_in():
+    """Fills in the login form and clicks the 'Log in' button"""
+    page = browser.page()
+    page.fill("#username", "maria")
+    page.fill("#password", "thoushallnotpass")
+    page.click("button:text('Log in')")
 
 
 def get_orders():
@@ -32,29 +40,24 @@ def get_orders():
     http.download(url="https://robotsparebinindustries.com/orders.csv", overwrite=True)
     """Read csv"""
     library = Tables()
-    orders = library.read_table_from_csv(
-        "orders.csv", columns=["name", "mail", "product"]
+    orders_csv = library.read_table_from_csv(
+        "orders.csv", columns=["Order number", "Head", "Body", "Legs", "Address"]
     )
+    orders = []
+    for order in orders_csv:
+        orders.append(order)
+
     return orders
 
 #def close_annoying_modal():
 
 def fill_and_submit_orders_form(orders):
-    """Fills in the sales data and click the 'Submit' button"""
+    """Fills in the orders data and click the 'Submit' button"""
     page = browser.page()
 
-    page.fill("#firstname", sales_rep["First Name"])
-    page.fill("#lastname", sales_rep["Last Name"])
-    page.select_option("#salestarget", str(sales_rep["Sales Target"]))
-    page.fill("#salesresult", str(sales_rep["Sales"]))
+    page.fill("#ordernumber", str(orders["Order number"]))
+    page.fill("#head", str(orders["Head"]))
+    page.select_option("#body", str(orders["Body"]))
+    page.fill("#legs", str(orders["Legs"]))
+    page.fill("#address", orders["Address"])
     page.click("text=Submit")
-    
-def fill_the_form(orders):
-    """Read data from excel and fill in the sales form"""
-    csv = Tables()
-    csv
-    worksheet = excel.read_worksheet_as_table("data", header=True)
-    excel.close_workbook()
-
-    for row in worksheet:
-        fill_and_submit_orders_form(row)
