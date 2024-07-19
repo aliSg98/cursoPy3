80e0c0bb0d98772a7db3cb4efa30829216e2c16d
aliSg98 <ali_sg@outlook.com>
2024-07-19 13:02:57 +0200

    robo2.5

--- robot2/tasks.py
+++ robot2/tasks.py
@@ -2,11 +2,11 @@ from robocorp.tasks import task
 from robocorp import browser, vault
 from RPA.HTTP import HTTP
 from RPA.Tables import Tables
-from RPA.Browser.Selenium import Selenium
+#from RPA.Browser.Selenium import Selenium
 import csv
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
+#from selenium import webdriver
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.by import By
 
 @task
 def order_robots_from_RobotSpareBin():
@@ -35,16 +35,16 @@ def get_orders():
     """Downloads csv file from the given URL"""
     http = HTTP()
     http.download(url="https://robotsparebinindustries.com/orders.csv", overwrite=True)
-
-    archivo = "orders.csv"
     """Read csv"""
-    orders_list = []
-    with open(archivo, 'r') as csvfichero:
-                orders = csv.DictReader(csvfichero)                
-                for row in orders:
-                    orders_list.append(row)
+    library = Tables()
+    orders_csv = library.read_table_from_csv(
+        "orders.csv", columns=["Order number", "Head", "Body", "Legs", "Address"]
+    )
+    orders = []
+    for order in orders_csv:
+        orders.append(order)
 
-    return orders_list
+    return orders
 
 def close_annoying_modal():
     page = browser.page()
@@ -53,15 +53,15 @@ def close_annoying_modal():
 
 def fill_the_form(orders):
     """Fills in the orders data and click the 'Submit' button"""
-    #page = browser.page()
-    browser = Selenium()
-    xpath = "//*[@id='head']"
-    head = browser.find_element(f'xpath:{xpath}')
-    browser.input_text
+    page = browser.page()
+    xpath_head = "//select[@id='head']]"
+    page.select_option(xpath_head,values=orders["Head"])
+    #browser.input_text(head,orders["Head"])
     #head = page.find_element(By.XPATH,"//*[@id="head"]"))
     #head.select_by_value(orders["Head"])
     #page.fill("#head", orders["Head"])
     #page.fill("#body", orders[2])
     #page.fill("#legs", str(orders[3]))
-    #page.fill("#address", orders["Address"])
+    xpath_address = "//select[@id='address']"
+    page.fill("#address", orders["Address"])
     #page.click("text=Submit")
